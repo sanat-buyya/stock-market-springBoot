857a8b134ae2ba20383d1fa3d3e2583a72caaf21
sanat-buyya <sanatbuyya@gmail.com>
2025-03-31 00:21:13 +0530

    Day-2

--- src/main/java/org/jsp/stock/controller/StockController.java
+++ src/main/java/org/jsp/stock/controller/StockController.java
@@ -1,40 +1,54 @@
 package org.jsp.stock.controller;
- 
- import org.jsp.stock.dto.User;
- import org.jsp.stock.service.StockService;
- import org.springframework.beans.factory.annotation.Autowired;
- import org.springframework.stereotype.Controller;
- import org.springframework.web.bind.annotation.GetMapping;
- import org.springframework.web.bind.annotation.ModelAttribute;
- import org.springframework.web.bind.annotation.PostMapping;
- 
- import jakarta.servlet.http.HttpSession;
- 
- @Controller
- public class StockController {
- 
- 	@Autowired
- 	StockService service;
- 
- 	@GetMapping("/")
- 	public String loadMain() {
- 		return "main.html";
- 	}
- 
- 	@GetMapping("/login")
- 	public String loadLogin() {
- 		return "login.html";
- 	}
- 
- 	@PostMapping("/login")
- 	public String login(@ModelAttribute User user, HttpSession session) {
- 		return service.login(user, session);
- 	}
- 
- 	@GetMapping("/logout")
- 	public String logout(HttpSession session) {
- 		session.removeAttribute("user");
- 		session.removeAttribute("admin");
- 		return "redirect:/";
- 	}
- }
+
+import java.time.LocalDate;
+
+import org.jsp.stock.dto.User;
+import org.jsp.stock.service.StockService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.validation.Valid;
+
+@Controller
+public class StockController {
+
+	@Autowired
+	StockService service;
+
+	@GetMapping("/")
+	public String loadMain() {
+		return "main.html";
+	}
+
+	@GetMapping("/login")
+	public String loadLogin() {
+		return "login.html";
+	}
+
+	@GetMapping("/register")
+	public String loadRegister(User user, Model model) {
+		model.addAttribute("user", user);
+		return "register.html";
+	}
+
+	@PostMapping("/register")
+	public String register(@ModelAttribute @Valid User user, BindingResult result) {
+		if(!user.getPassword().equals(user.getConfirmPassword()))
+			result.rejectValue("confirmPassword", "error.confirmPassword", "* Password and Confirm Password are Not Matching");
+		if(LocalDate.now().getYear()-user.getDob().getYear()<18)
+			result.rejectValue("dob", "error.dob", "* You should be 18+ to Create Account here");
+		
+		if (result.hasErrors()) {
+			return "register.html";
+		} else {
+			System.err.println(user);
+			return "otp.html";
+		}
+	}
+
+}
